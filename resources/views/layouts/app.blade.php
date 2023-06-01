6c0591bb7cf493ce22a77b17970d2bca0fc45641
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-06-01 11:07:36 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Atop Clean</title>
+</head>
+<body>
+    @include('components.sidebar')
+    @yield('main')
+    <script src="https://cdn.tailwindcss.com"></script>
+</body>
+</html>
