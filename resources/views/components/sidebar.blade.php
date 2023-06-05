b365ba91a6cd760d8c0f237321b1b97578d5396e
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-06-05 08:37:45 +0700

    sidebar design

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,37 @@
+<div class="bg-[#55ACEE] flex flex-col w-1/5 h-screen">
+    {{-- Image Logo --}}
+    <div>
+        <img src="{{ asset('logo-atop.png') }}" alt="">
+    </div>
+
+    {{-- Sidebar Item --}}
+    <div class="flex flex-col">
+
+        <a href="">
+            <div class="flex items-center p-3 justify-center gap-2 hover:bg-[#226699] w-full">
+                <i class="bi bi-journal-text text-2xl"></i>
+                <p class="font-medium">Daftar Pesanan</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="flex items-center p-3 justify-center gap-2 hover:bg-[#226699] w-full">
+                <i class="bi bi-file-plus text-2xl"></i>
+                <p class="font-medium">Tambah Pesanan</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="flex items-center p-3 justify-center gap-2 hover:bg-[#226699] w-full">
+                <i class="bi bi-clock-history text-2xl"></i>
+                <p class="font-medium">Histori</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="flex items-center p-3 justify-center gap-2 hover:bg-[#226699] w-full">
+                <i class="bi bi-bar-chart text-2xl"></i>
+                <p class="font-medium">Laporan</p>
+            </div>
+        </a>
+
+    </div>
+
+</div>
