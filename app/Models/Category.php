07cfb5758fede66a86ba2e7603890dd2f53376be
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-06-09 13:40:02 +0700

    relation between category and treatment

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * Get all of the treatments for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
 }
