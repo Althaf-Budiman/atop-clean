17f2ac0063e80b756923220371be6da65cc924f4
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-06-13 10:01:40 +0700

    show data in index

--- app/Http/Livewire/DaftarPesanan.php
+++ app/Http/Livewire/DaftarPesanan.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Laundry;
 use Livewire\Component;
 
 class DaftarPesanan extends Component
 {
+    public $laundries;
+
+    public function mount()
+    {
+        $this->laundries = Laundry::all();
+    }
+
     public function render()
     {
         return view('livewire.daftar-pesanan');
--- resources/views/livewire/daftar-pesanan.blade.php
+++ resources/views/livewire/daftar-pesanan.blade.php
@@ -1,3 +1,29 @@
-<div>
-    {{-- Stop trying to control. --}}
+<div class="flex p-5 flex-wrap gap-5">
+
+    @foreach ($laundries as $laundry)
+    {{-- Card --}}
+        <div class="rounded-md shadow-lg w-60 overflow-hidden">
+            @isset($laundry->gambar)
+                <img src="{{ asset('storage/' . $laundry->gambar) }}" class="h-40 w-full object-cover">
+                @else
+                <div class="h-40 w-full object-cover bg-gray-500 text-black font-semibold text-center leading-[10rem]">No Image</div>
+            @endisset
+            <h2 class="font-semibold text-xl text-center pt-2">{{ $laundry->nama }}</h2>
+            <div class="flex">
+                <p class="font-medium text-[13px] text-gray-600 px-2 pt-2 whitespace-nowrap text-ellipsis ">Customer: <b>{{ $laundry->customer }}</b></p>
+            </div>
+            <div class="flex">
+                <p class="font-medium text-[13px] text-gray-600 px-2">Tanggal Ambil: <b>{{ $laundry->tanggal_ambil }}</b></p>
+            </div>
+            <div class="flex">
+                <p class="font-medium text-[13px] text-gray-600 px-2">Category: <b>{{ $laundry->category }}</b></p>
+            </div>
+            <div class="flex justify-between">
+                <p class="font-medium text-[13px] text-gray-600 pl-2 whitespace-nowrap text-ellipsis">Treatment: <b>{{ preg_replace("/\([^()]*\)/", "", $laundry->treatment); }}</b></p>
+                <p class="font-medium text-lg pr-2 text-green-600"><b>Rp. {{ number_format($laundry->harga, 0, ".", "."); }}</b></p>
+            </div>
+        </div>
+    {{-- End Card --}}
+    @endforeach
+
 </div>
