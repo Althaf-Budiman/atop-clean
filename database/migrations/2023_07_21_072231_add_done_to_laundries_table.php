353437a1f923d52d6d15b40287976f8f478ac821
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-07-21 14:30:44 +0700

    tambahkan kolom done pada tabel laundries

---
+++ database/migrations/2023_07_21_072231_add_done_to_laundries_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('laundries', function (Blueprint $table) {
+            // false itu 0, true itu 1.
+            $table->boolean('done')->default(false)->after('gambar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('laundries', function (Blueprint $table) {
+            $table->dropColumn('done');
+        });
+    }
+};
